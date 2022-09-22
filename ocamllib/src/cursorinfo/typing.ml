542bb30e0309afe3501f62f7088fa84600214916
Eric Fan <yikaifan@umich.edu>
2022-09-22 15:27:32 -0400

    Add typing for patterns

--- ocamllib/src/cursorinfo/typing.ml
+++ ocamllib/src/cursorinfo/typing.ml
@@ -27,114 +27,117 @@ let rec get_common_type (t1 : Type.p_t) (t2 : Type.p_t) : Type.p_t option =
     | _ -> None
   else None
 
+let rec pattern_type (p : Pattern.p_t) : Type.p_t =
+    match p with
+    | Const (Int _) -> Type.Int
+    | Const (Bool _) -> Type.Bool
+    | Const (Nil _) -> Type.List Type.Hole
+    | Var _ -> Type.Hole
+    | List (p1, p2) -> 
+        let t1 = pattern_type p1 in
+        let t2 = pattern_type p2 in
+        begin match t2 with
+        | Type.Hole -> Type.List t1
+        | Type.List t2 -> 
+            let t = get_common_type t1 t2 in
+            begin match t with
+            | Some t -> Type.List t
+            | None -> raise (Failure "List pattern type error")
+            end
+        | _ -> raise (Failure "List pattern type error")
+        end
+    | Wild -> Type.Hole
+
+(* Return Some t if t is the common type of the rules. Otherwise, return None *)
+let pattern_common_type (rules : (Pattern.t * Expr.t) list) : Type.p_t option = 
+    let pattern_common_type_aux (acc : Type.p_t option) (rule : Pattern.t * Expr.t) : Type.p_t option =
+        match acc with
+        | None -> None
+        | Some t -> 
+            let (p, _) = rule in
+            get_common_type t (pattern_type (Pattern.strip p))
+    in
+    List.fold_left pattern_common_type_aux (Some Type.Hole) rules
+
 let rec synthesis (context : Context.t) (e : Expr.t) : Type.p_t option =
   (*given an expression and its type-context, infer its type by looking
      at its children, if possible *)
-  match e.node with
-  (*match epxression based on type (and operation for unops and binops)*)
-  | EVar x -> Context.lookup context x
-  | EConst c -> 
-    begin match c with
-    | Int _ -> Some Int
-    | Bool _ -> Some Bool
-    | Nil -> Some (List Hole)
-    end
-  | EUnOp (OpNeg, arg) ->
-      (* negation: if child is int, expr has same type *)
-      if analysis context arg Int then Some Int else None
-  | EBinOp (argl, (OpPlus | OpMinus | OpTimes | OpDiv), argr) ->
-      (*arithmetic operations: if we see an int, return an int *)
-      if analysis context argl Int && analysis context argr Int
-      then Some Int
-      else None
-  | EBinOp (argl, (OpGt | OpGe | OpLt | OpLe | OpEq | OpNe), argr) ->
-      (*comparasons: int-> bool*)
-      if analysis context argl Int && analysis context argr Int
-      then Some Bool
-      else None
-  | EBinOp (arrow, OpAp, arg) -> (
-      match synthesis context arrow with
-      | Some (Arrow (in_t, out_t)) ->
-          if analysis context arg in_t then Some out_t else None
-      | Some Hole -> if analysis context arg Hole then Some Hole else None
-      | _ -> None)
-  | EBinOp (hd, OpCons, tl) -> (
-      match synthesis context tl with
-      | Some (List list_t) ->
-          if analysis context hd list_t then Some (List list_t) else None
-      | Some Hole -> if analysis context hd Hole then Some (List Hole) else None
-      | _ -> None)
-  | EPair (l_pair, r_pair) -> (
-      match (synthesis context l_pair, synthesis context r_pair) with
-      | Some l_t, Some r_t -> Some (Prod (l_t, r_t))
-      | _ -> None)
-  | EIf (econd, ethen, eelse) ->
-      if analysis context econd Bool
-      then
+    match e.node with
+    (*match epxression based on type (and operation for unops and binops)*)
+    | EVar x -> Context.lookup context x
+    | EConst c -> 
+        begin match c with
+        | Int _ -> Some Int
+        | Bool _ -> Some Bool
+        | Nil -> Some (List Hole)
+        end
+    | EUnOp (OpNeg, arg) ->
+        (* negation: if child is int, expr has same type *)
+        if analysis context arg Int then Some Int else None
+    | EBinOp (argl, (OpPlus | OpMinus | OpTimes | OpDiv), argr) ->
+        (*arithmetic operations: if we see an int, return an int *)
+        if analysis context argl Int && analysis context argr Int
+        then Some Int
+        else None
+    | EBinOp (argl, (OpGt | OpGe | OpLt | OpLe | OpEq | OpNe), argr) ->
+        (*comparasons: int-> bool*)
+        if analysis context argl Int && analysis context argr Int
+        then Some Bool
+        else None
+    | EBinOp (arrow, OpAp, arg) -> (
+        match synthesis context arrow with
+        | Some (Arrow (in_t, out_t)) ->
+            if analysis context arg in_t then Some out_t else None
+        | Some Hole -> if analysis context arg Hole then Some Hole else None
+        | _ -> None)
+    | EBinOp (hd, OpCons, tl) -> (
+        match synthesis context tl with
+        | Some (List list_t) ->
+            if analysis context hd list_t then Some (List list_t) else None
+        | Some Hole -> if analysis context hd Hole then Some (List Hole) else None
+        | _ -> None)
+    | EPair (l_pair, r_pair) -> (
+        match (synthesis context l_pair, synthesis context r_pair) with
+        | Some l_t, Some r_t -> Some (Prod (l_t, r_t))
+        | _ -> None)
+    | EIf (econd, ethen, eelse) ->
+        if analysis context econd Bool
+        then
         let tthen = synthesis context ethen in
         let telse = synthesis context eelse in
         match (tthen, telse) with
         | Some tthen, Some teelse -> get_common_type tthen teelse
         | _ -> None
-      else None
-  | ELet (varn, dec, body) -> (
-      match synthesis context dec with
-      | Some var_t -> synthesis (Context.extend context (varn, var_t)) body
-      | _ -> None)
-  | EFun (varn, vart, body) -> (
-      let vart = Type.strip vart in
-      match synthesis (Context.extend context (varn, vart)) body with
-      | Some outtype -> Some (Arrow (vart, outtype))
-      | _ -> None)
-  | EFix (varn, vart, body) ->
-      let vart = Type.strip vart in
-      if analysis (Context.extend context (varn, vart)) body vart
-      then Some vart
-      else None
-  | EHole -> Some Hole
-  | EMatch (escrut, rules) -> 
-    begin match synthesis context escrut with
-    | Some tscrut -> 
-        let rec rule_common_type acc rules =
-            match rules with
-            | [] -> Some acc
-            | (p, e) :: tl -> 
-                match p with
-                | Expr.PConst c ->
-                    let t = 
-                        match c with
-                        | Int _ -> Type.Int
-                        | Bool _ -> Type.Bool
-                        | Nil -> Type.List Hole
-                    in
-                    if Type.consistent t tscrut then 
-                        match synthesis context e with
-                        | Some t -> 
-                            begin match get_common_type acc t with
-                            | Some t -> rule_common_type t tl
-                            | None -> None
-                            end
-                        | None -> None
-                    else
-                        None
-                | Expr.PVar var ->
-                    begin match synthesis (Context.extend context (var, tscrut)) e with
-                    | Some t -> rule_common_type t tl
-                    | None -> None
-                    end
-                | Expr.PWild -> 
-                    begin match synthesis context e with
-                    | Some t -> 
-                        begin match get_common_type acc t with
-                        | Some t -> rule_common_type t tl
-                        | None -> None
-                        end
-                    | None -> None
-                    end              
-        in
-        rule_common_type Hole rules
-    | None -> None
-    end
+        else None
+    | ELet (varn, dec, body) -> (
+        match synthesis context dec with
+        | Some var_t -> synthesis (Context.extend context (varn, var_t)) body
+        | _ -> None)
+    | EFun (varn, vart, body) -> (
+        let vart = Type.strip vart in
+        match synthesis (Context.extend context (varn, vart)) body with
+        | Some outtype -> Some (Arrow (vart, outtype))
+        | _ -> None)
+    | EFix (varn, vart, body) ->
+        let vart = Type.strip vart in
+        if analysis (Context.extend context (varn, vart)) body vart
+        then Some vart
+        else None
+    | EHole -> Some Hole
+    | EMatch (escrut, rules) -> 
+        begin match synthesis context escrut with
+        | Some tscrut -> 
+            let t_rules = pattern_common_type rules in
+            begin match t_rules with
+            | Some t_rules -> 
+                if Type.consistent tscrut t_rules 
+                then 
+                else None
+            | None -> None
+            end
+        | None -> None
+        end
+    | EList ()
 
 and analysis (context : Context.t) (e : Expr.t) (targ : Type.p_t) : bool =
   (* given an epxression and an expected type,
