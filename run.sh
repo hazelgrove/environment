3772666365767af2a9e4b16a1b13effdfd9d3522
ZacharyEichen <zachary.eichen@gmail.com>
2023-06-23 21:38:25 -0400

    added logic to run.sh to select a single gpu

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #! /usr/bin/env bash
-
 name=$1
 mount_dir=$2
+gpus="all"
+if ! [ -z "$4" ]
+then
+    gpus="\"device=$4\""
+fi
+echo "using gpus: $gpus"
 docker build -t "$name" .
 docker run --rm -it -d \
 	--env-file .env \
 	--shm-size=10.24gb \
 	--name "$name" \
-	--gpus all \
+	--gpus $gpus \
 	--network="host" \
 	-h="$(hostname -s)" \
 	-e TERM=xterm-256color \
