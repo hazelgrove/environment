3da23f67a7fe43655a2c554341f0c57bd9f2258c
ZacharyEichen <zachary.eichen@gmail.com>
2023-06-27 14:17:45 -0400

    allow gpu number to be specified in run.sh and visualize.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/env bash
 name=$1
-mount_dir=$2
-gpus="all"
+mount_dir="/RL_env/save"
+
 if ! [ -z "$4" ]
 then
     gpus="\"device=$4\""
+else 
+    gpus="all"
 fi
+echo "one:$1  two:$2 three:$3 four:$4 five:$5"
 echo "using gpus: $gpus"
 docker build -t "$name" .
+
 docker run --rm -it -d \
 	--env-file .env \
 	--shm-size=10.24gb \
@@ -17,5 +21,5 @@ docker run --rm -it -d \
 	-h="$(hostname -s)" \
 	-e TERM=xterm-256color \
 	-v rl_checkpoint:"$mount_dir" \
-	"$name" "${@:3}" "$mount_dir"
+	"$name" "${@:3:1}" "$mount_dir"
 docker logs -f "$name"
--- visualize.sh
+++ visualize.sh
@@ -2,15 +2,24 @@
 
 name=$1
 mount_dir=$2
+
+gpus="all"
+if ! [ -z "$5" ]
+then
+    gpus="\"device=$5\""
+fi
+echo "using gpus: $gpus"
+
+
 docker build -t "$name" .
 docker run --rm -it \
 	--env-file .env \
 	--shm-size=10.24gb \
 	--name "$name" \
-	--gpus all \
+	--gpus $gpus \
 	--network="host" \
 	-h="$(hostname -s)" \
 	-e TERM=xterm-256color \
 	-v rl_checkpoint:"$mount_dir" \
 	--entrypoint /RL_env/visualize_entrypoint.sh \
-	"$name" "${@:3}" "$mount_dir"
+	"$name" "${@:3:2}" "$mount_dir"
