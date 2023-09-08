4272f27bb1bac934af088ba1d26c43e829787d0e
ZachEichen <zachary.eichen@gmail.com>
2023-09-07 23:24:50 -0400

    made modification to erics test gen code to allow arbitrary inputs to be passed

---
+++ scripts/compile_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+Script that assembles sets of tests + params from many different sources to create a cohesive test script 
+
+Sample folders contain the following 
+	- A test folder, of the format accepted by the environment 
+    - A train folder, of the format acceepted by the environment 
+    - A params.yaml that covers a test for the two  
+    
+"""
